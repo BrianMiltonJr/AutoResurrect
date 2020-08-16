64e6f54c53e50e5c8b0ae90aa6964040cb5f9187
BrianMiltonJr <harlanbmiltonjr@gmail.com>
2020-08-15 21:30:37 -0500

    New Init
    We bootstrap our application here, and put all global var's in
    Our namespace

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 --addon AutoResurrect: index
-local AutoResurrect, NS = ...;
+local addonName, NS = ...;
+
+NS.Frame = CreateFrame("Frame");
+
+--The messages our addon spits out
 NS.Messages = {
   ["Thanks"] = {
     ["Resurrect"] = "Thank you for Resurrecting me.",
@@ -11,63 +15,12 @@ NS.Messages = {
   ["Language"] = GetDefaultLanguage("player"),
   ["Channel"] = nil
 };
+
+--Whether certain features are enabled of disabled
 NS.Toggles = {
-  ["CombatResurrect"] = true,
-  ["Messages"] = true,
-  ["CombatStatus"] = false,
-  ["Enabled"] = true,
-}
-
-local AutoResurrect = CreateFrame("Frame");
-
--- Handily checks if we can send a message
-local function SendMessage(msg, channel, language, target)
-  if Toggle.Messages then SendChatMessage(msg, channel, language, target); end
-end
-
--- Sets our toggle to false or true if status == "off" or "on". If Status == nil the we flip the toggle
-local function FlipToggle(key, status)
-  if Toggles[key] == nil then return nil; end
-
-  if status == "on" then Toggles[key] = true; elseif status == "off" then Toggles[key] = false; else Toggles[key] = not Toggles[key]; end
-end
-
---Splits our Command Arguments. Currently only does Space.
-local function SplitArgs(txt, delimiter)
-  local args = {};
-  for arg in string.gmatch(txt, "%S+") do table.insert(args, arg); end
-  return {args[1], table.unpack(args, 2)};
-end
-
-
-
-
-
-
-Commands = {
-  ["status"] = function(self, ...)
-    local command = ...;
-    FlipToggle("Enabled", command);
-  end,
-  ["messages"] = function(self, ...)
-    local command = ...;
-    FlipToggle("Messages", command);
-  end,
-  ["combat"] = function(self, ...)
-    local command = ...;
-    FlipToggle("CombatResurrect", command);
-  end,
-}
-
-
-
---Setup our command aliases
-SLASH_AUTORESURRECT1 = "/ar";
-SLASH_AUTORESURRECT2 = "/autoresurrect";
-
---Setup our command Handler
-SlashCmdList["AUTORESURRECT"] = function(self, txt)
-  local args = SplitArgs(txt);
-  
-  Commands[args[1]](args[2])
-end
+  ["CombatResurrect"] = true, --Accept Rez during Combat
+  ["Messages"] = true, --Post messages in chat
+  ["CombatStatus"] = false, --Are we in combat
+  ["Enabled"] = true, --Is the addon enabled
+  ["Debug"] = true, --Do we print Debug information to chat
+};
